e2a1e4012a90f4820be00f954575ba28977812f6
Yusuke Yagyu <gyugyu@pepabo.com>
2019-06-26 12:33:57 +0900

    fix

--- src/command.ts
+++ src/command.ts
@@ -1,9 +1,15 @@
+#!/usr/bin/env node
+
 import program from 'commander'
 import path from 'path'
 import index from './index'
 
 const split = (value: string) => {
   return value.split(',')
+}
+
+const resolve = (value: string) => {
+  return path.resolve(value)
 }
 
 interface Options {
@@ -15,8 +21,8 @@ interface Options {
 program
   .version(require('../package.json').version)
   .arguments('<scope>')
-  .option('-d, --dir <dir>', 'directory where credentials.json placed', path.resolve, process.cwd())
-  .option('-o, --out-dir <dir>', 'out dir where token.json will be dumped', path.resolve, process.cwd())
+  .option('-d, --dir <dir>', 'directory where credentials.json placed', resolve, process.cwd())
+  .option('-o, --out-dir <dir>', 'out dir where token.json will be dumped', resolve, process.cwd())
   .option('-s, --scopes <scopes>', 'optional scopes', split, [])
   .action((scope: string, options: Options) => {
     options.scopes.push(scope)
