4e045ba6248ea9c6ccbbeafa1790cd1d7929b9e3
Yusuke Yagyu (gyugyu) <gyugyu@pepabo.com>
2022-11-15 21:49:55 +0900

    support path joining from cwd

--- src/command.ts
+++ src/command.ts
@@ -4,12 +4,14 @@ import { program } from 'commander'
 import path from 'path'
 import index from './index'
 
+const cwd = process.cwd()
+
 const split = (value: string) => {
   return value.split(',')
 }
 
-const resolve = (value: string) => {
-  return path.resolve(value)
+const join = (value: string) => {
+  return path.join(cwd, value)
 }
 
 interface Options {
@@ -21,8 +23,8 @@ interface Options {
 program
   .version(require('../package.json').version)
   .arguments('<scope>')
-  .option('-d, --dir <dir>', 'directory where credentials.json placed', resolve, process.cwd())
-  .option('-o, --out-dir <dir>', 'out dir where token.json will be dumped', resolve, process.cwd())
+  .option('-d, --dir <dir>', 'directory where credentials.json placed', join, cwd)
+  .option('-o, --out-dir <dir>', 'out dir where token.json will be dumped', join, cwd)
   .option('-s, --scopes <scopes>', 'optional scopes', split, [])
   .action((scope: string, options: Options) => {
     options.scopes.push(scope)
